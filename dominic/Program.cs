4a9e45d7929bcef79680b9d6522792b25a4d2fc9
BCAS-Student01 <BCAS-Student01@DESKTOP-2AGPVOD>
2022-11-05 13:39:59 +0800

    Add project files.

---
+++ dominic/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+public interface IPaintTool
+{
+    void MouseUp();
+    void MouseOver();
+}
+
+public class IPencilTool : IPaintTool
+{
+    public void MouseOver()
+    {
+        Console.WriteLine("Show pencil icon");
+    }
+
+    public void MouseUp()
+    {
+        Console.WriteLine("Draw a line.");
+    }
+}
+
+public class ISquare : IPaintTool
+{
+    public void MouseOver()
+    {
+        Console.WriteLine("Show square icon");
+    }
+
+    public void MouseUp()
+    {
+        Console.WriteLine("Draw a square.");
+    }
+}
+
+public class IEraser : IPaintTool
+{
+    public void MouseOver()
+    {
+        Console.WriteLine("Show an eraser icon");
+    }
+
+    public void MouseUp()
+    {
+        Console.WriteLine("Erase the selected object.");
+    }
+}
